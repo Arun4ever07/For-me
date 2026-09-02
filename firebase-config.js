84eafca59b5b8d174f9b7ce1eaae52379154b60f
Arun4ever07 <arundamor483@gmail.com>
2026-09-02 13:23:17 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   // IMPORTANT:
   // Replace the value below with the EXACT URL from
   // Firebase → Build → Realtime Database.
-  databaseURL: "PASTE_YOUR_REALTIME_DATABASE_URL_HERE",
+  databaseURL: "https://console.firebase.google.com/u/1/project/forme-49a8f/database/forme-49a8f-default-rtdb/data/~2F",
 
   projectId: "forme-49a8f",
   storageBucket: "forme-49a8f.firebasestorage.app",
